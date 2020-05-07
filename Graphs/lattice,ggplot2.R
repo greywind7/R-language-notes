6fbf2d8acc8f29987247946c8f0098d5781af0ce
greywind7 <ritik.aggr@gmail.com>
2020-05-07 18:57:15 +0530

    ggplot2 started

--- Graphs/lattice,ggplot2.R
+++ Graphs/lattice,ggplot2.R
@@ -18,4 +18,24 @@ airquality <- transform(airquality, Month = factor(Month))
 xyplot(Ozone~Wind | Month, data = airquality, layout = c(5,1))
 # lattice plots return objects of a class trellis or smth which can be stored in a variable
 # youcan make a custom panel function
+
+# ***********************ggplot2***********************
+library(ggplot2)
+# qplot() is the basic function of ggplot2
+# color to give different colors. shape to give different shapes
+qplot(displ,hwy, data = mpg)
+qplot(displ,hwy, data = mpg, color = drv)
+# you can make a histogram ussing a single data
+qplot(hwy,data = mpg, fill = drv)
+# facets argument helps you to make side by side plots
+# variable to the right means one col, left means two col
+# replace . to enter label
+qplot(displ,hwy, data = mpg, facets = .~drv)
+qplot(hwy,data = mpg, facets = drv~.)
+# geom_smooth() to make linear lines
+qplot(displ,hwy, data = mpg, color = drv) + geom_smooth(method = "lm")
+# geom attribute makes lines of sorts ig
+# Saving plots in higher qualities
+plot <- qplot(log(hwy),data = mpg, geom = "density", color = drv)
+ggsave(plot, file = "myplot.png", dpi = 1000)
 # *************************************************************
