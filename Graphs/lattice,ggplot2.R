8bfbdd65a67dd66c4c08b42f5748cc50d6826c77
greywind7 <ritik.aggr@gmail.com>
2020-05-07 19:47:57 +0530

    added ggplot() fn

--- Graphs/lattice,ggplot2.R
+++ Graphs/lattice,ggplot2.R
@@ -21,6 +21,14 @@ xyplot(Ozone~Wind | Month, data = airquality, layout = c(5,1))
 
 # ***********************ggplot2***********************
 library(ggplot2)
+# Basic components of ggplot2 plot
+# + A data frame
+# + Aesthetic mappings
+# + geoms -- objects like points, lines, shapes
+# + facets -- for conditional plots
+# + stats -- stat. trans. like binning, quantiles, smoothing
+# + scales -- what scale an aesthetic map uses
+# + coordinate systems
 # qplot() is the basic function of ggplot2
 # color to give different colors. shape to give different shapes
 qplot(displ,hwy, data = mpg)
@@ -34,8 +42,24 @@ qplot(displ,hwy, data = mpg, facets = .~drv)
 qplot(hwy,data = mpg, facets = drv~.)
 # geom_smooth() to make linear lines
 qplot(displ,hwy, data = mpg, color = drv) + geom_smooth(method = "lm")
-# geom attribute makes lines of sorts ig
 # Saving plots in higher qualities
 plot <- qplot(log(hwy),data = mpg, geom = "density", color = drv)
 ggsave(plot, file = "myplot.png", dpi = 1000)
+
+# Using ggplot to systematically make a plot
+g <- ggplot(mpg, aes(displ,hwy))
+# adding points
+g <- g + geom_point(aes(color = drv),alpha = 1/2)
+# Adding more layers: smooth
+g + geom_smooth()
+g <- g + geom_smooth(method = "lm")
+# adding facets
+g + facet_grid(.~drv)
+# annotations using xlab(), ylab(), labs(), ggtitle()
+# theme() to make changes globally
+# two std themes theme_gray() theme_bw()
+# modyfying aesthetics
+# color, size and alpha(transparency) in points
+# linetype, method, se in smooth
+# base_family in different themes
 # *************************************************************
