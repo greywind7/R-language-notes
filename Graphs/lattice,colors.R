10432360cf90d28762dad25cfc1ae3f95ecdec86
greywind7 <ritik.aggr@gmail.com>
2020-05-08 14:09:17 +0530

    Added working colors

--- Graphs/lattice,colors.R
+++ Graphs/lattice,colors.R
@@ -1,6 +1,7 @@
 # ***********************Lattice plots***********************
 # Margins and spacing are automatically adjusted
 library(lattice)
+library(ggplot2)
 # Makes plot in single calls
 # Main functions are
 # + xyplot -- main fn for scatterplots
@@ -23,14 +24,39 @@ xyplot(Ozone~Wind | Month, data = airquality, layout = c(5,1))
 # lattice plots return objects of a class trellis or smth which can be stored in a variable
 # youcan make a custom panel function
 # example:
+y = rnorm(100,100,25)
+x = rnorm(100,100,5)
+f = factor(sample(c("hi","lo"),100, replace = T))
 p <- xyplot(y ~ x | f, panel = function(x, y, ...) {
     panel.xyplot(x, y, ...)  ## First call the default panel function for 'xyplot'
     panel.abline(h = median(y), lty = 2)  ## Add a horizontal line at the median
 })
+print(p)
 # Multiple panels example with ggplot2 diamonds data
 myxlab <- "Carat"
 myylab <- "Price"
 mymain <- "Diamonds are Sparkly!"
 xyplot(price~carat|color*cut,data=diamonds,strip=FALSE,pch=20,xlab=myxlab,ylab=myylab,main=mymain)
 # strip labels the plot
-# *************************************************************
+
+# ***********************Working with colors***********************
+library(grDevices)
+colors() # Lists all the available colors as a string
+# colorRamp() takes a list of colors and creates a fn which blends taking in args from 0 to 1
+# the object made by this can take more than one argument
+# colorRampPalette() works similarly but takes integer values
+# These are three length, 4th can be given by alpha argument
+# This integer specified makes the function return a hex color vector
+# Taken from documentation to display the colors
+showMe <- function(cv){
+    myarg <- deparse(substitute(cv))
+    z<- outer( 1:20,1:20, "+")
+    obj<- list( x=1:20,y=1:20,z=z )
+    image(obj, col=cv, main=myarg  )
+}
+# You can take the rgb() function to take the rgb values and optional alpha to make colors
+library(RColorBrewer)
+display.brewer.all() # displays all the colors
+display.brewer.pal(n = 3, name = "Dark2") # Displays a specific pallete
+# brewer.pal() takes a number and a category and returns a palette. Can be used with above fns
+# *****************************************************************
