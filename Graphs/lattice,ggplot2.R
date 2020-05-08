345902da35e95a70c801b8853c71812d78e6cc86
greywind7 <ritik.aggr@gmail.com>
2020-05-08 13:12:31 +0530

    Completed latticeplot

--- Graphs/lattice,ggplot2.R
+++ Graphs/lattice,ggplot2.R
@@ -1,4 +1,5 @@
 # ***********************Lattice plots***********************
+# Margins and spacing are automatically adjusted
 library(lattice)
 # Makes plot in single calls
 # Main functions are
@@ -9,15 +10,29 @@ library(lattice)
 # + dotplot -- plot dots on violin strings
 # + splom -- scatterplot matrix
 # + levelplot, contourplot -- plotting image data
+# The attributes of base plots seem to apply
 
 # xyplot(y ~x | f * g, data)
+# * represents the intersection
 # This is a formula notation. x,y are axes, and f,g are factors
 xyplot(Ozone ~ Wind, data = airquality)
 # multiple graphs
 airquality <- transform(airquality, Month = factor(Month))
+# This could have been skipped using as.factor()
 xyplot(Ozone~Wind | Month, data = airquality, layout = c(5,1))
 # lattice plots return objects of a class trellis or smth which can be stored in a variable
 # youcan make a custom panel function
+# example:
+p <- xyplot(y ~ x | f, panel = function(x, y, ...) {
+    panel.xyplot(x, y, ...)  ## First call the default panel function for 'xyplot'
+    panel.abline(h = median(y), lty = 2)  ## Add a horizontal line at the median
+})
+# Multiple panels example with ggplot2 diamonds data
+myxlab <- "Carat"
+myylab <- "Price"
+mymain <- "Diamonds are Sparkly!"
+xyplot(price~carat|color*cut,data=diamonds,strip=FALSE,pch=20,xlab=myxlab,ylab=myylab,main=mymain)
+# strip labels the plot
 
 # ***********************ggplot2***********************
 library(ggplot2)
