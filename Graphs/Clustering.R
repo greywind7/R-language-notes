e8eaec590e6e9426d858a1e1517b2fa9dd58c153
greywind7 <ritik.aggr@gmail.com>
2020-05-12 12:31:05 +0530

    Added hierarchal clustering

---
+++ Graphs/Clustering.R
@@ -0,0 +1,7 @@
+library(fields)
+
+# dist() calculates the pairwise distances
+# hclust() takes the pairwise dist. variable and gives a dendogram
+# To view your dendogram you need plot() to plot it
+# plot with as.dendogram() for a neater one
+# heatmap() creates a heatmap
